505cfd9bb66e25e000abf8db14600cf7f2b27110
FLinLan <maxlan0987@gmail.com>
2025-04-24 18:34:10 -0700

    intialized all block components except for SPI and configured the pins

---
+++ CAN_Bridge.cydsn/main.c
@@ -0,0 +1,256 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+#include "cyapicallbacks.h"
+#include "CAN_Stuff.h"
+#include "FSM_Stuff.h"
+#include "HindsightCAN/CANLibrary.h"
+#define UNKNOWN_PACKET 0xFFFF
+
+// testing out the headers
+#include "cytypes.h"
+#include "cyfitter.h"
+#include "cydevice_trm.h"
+
+// LED stuff
+volatile uint8_t CAN_time_LED = 0;
+volatile uint8_t ERROR_time_LED = 0;
+
+// UART stuff
+char txData[TX_DATA_SIZE];
+
+// CAN stuff
+CANPacket can_recieve;
+CANPacket can_send;
+uint8 address = 0;
+char8 uart_tx[64];
+char8 uart_rx[64];
+CANPacket can_tx;
+CANPacket can_rx;
+
+uint8 uart_rx_len = 0;
+
+
+
+CY_ISR(Period_Reset_Handler) {
+    CAN_time_LED++;
+    ERROR_time_LED++;
+
+    if (ERROR_time_LED >= 3) {
+        LED_ERR_Write(OFF);
+    }
+    if (CAN_time_LED >= 3) {
+        LED_CAN_Write(OFF);
+    }
+}
+
+/*
+CY_ISR(Button_1_Handler) {
+    LED_DBG_Write(!LED_DBG_Read());
+}
+*/
+
+uint8 s2x(char8 c) {
+    switch(c) {
+        case '1': return 1; case '2': return 2;
+        case '3': return 3; case '4': return 4;
+        case '5': return 5; case '6': return 6;
+        case '7': return 7; case '8': return 8;
+        case '9': return 9; case 'A': return 10;
+        case 'B': return 11;case 'C': return 12;
+        case 'D': return 13;case 'E': return 14;
+        case 'F': return 15;case 'a': return 10;
+        case 'b': return 11;case 'c': return 12;
+        case 'd': return 13;case 'e': return 14;
+        case 'f': return 15;default:  return 0;
+    }
+}
+
+void parseLine(CANPacket* p, char8 line[], int length) {
+    uint16 pr = (uint16) s2x(line[0]) << 10;
+    uint16 dg = (uint16) s2x(line[2]) << 6;
+    uint16 sn = (uint16) s2x(line[4]) << 4 | s2x(line[5]);
+    p->id = (pr | dg | sn);
+    
+    int i = 0;
+    int j = 8;
+    while (j < length && i < 8) {
+        p->data[i] = (s2x(line[j-1])<<4) | s2x(line[j]);
+        j += 3;
+        i++;
+    }
+    p->dlc = i;
+}
+
+uint32_t decodeFromBytes(int msb_index, int lsb_index, uint8_t data[]) {
+    uint32_t result = 0;
+    if (msb_index < lsb_index)
+        for (int i = msb_index; i <= lsb_index; i++)
+            result = result << 8 | data[i];
+    else
+        for (int i = msb_index; i >= lsb_index; i--)
+            result = (result | data[i]) << 8;
+    return result;
+}
+
+void sprintCANPacket(CANPacket* packet, char* buffer) {
+    uint8 pri = (packet->id & 0x400) >> 10;
+    uint8 dg = (packet->id & 0x3C0) >> 6;
+    uint8 sn = (packet->id & 0x03F) >> 0;
+
+    int len = sprintf(buffer, "%01X %02X %02X ", pri, dg, sn);
+    for(int i = 0; i < packet->dlc; i++)
+        len += sprintf(buffer+len," %02X", packet->data[i]);
+
+    sprintf(buffer+len,"\r\n");
+}
+
+
+int main(void)
+{ 
+    // intialize the two CAN blocks here
+    Initialize();
+    
+    // CAN_Start();
+    // CAN_1_Start();
+    int err;
+    
+    // we can read CAN packets and pass it into uart first
+    for(;;)
+    {
+        /*
+        err = 0;
+        switch(GetState()) {
+            case(UNINIT):
+                SetStateTo(CHECK_CAN);
+                break;
+            case(CHECK_CAN):
+                if (!PollAndReceiveCANPacket(&can_recieve)) {
+                    LED_CAN_Write(ON);
+                    CAN_time_LED = 0;
+                    err = ProcessCAN(&can_recieve, &can_send);
+                }
+                if (GetMode() == MODE1)
+                    SetStateTo(DO_MODE1);
+                else 
+                    SetStateTo(CHECK_CAN);
+                break;
+            case(DO_MODE1):
+                // mode 1 tasks
+                SetStateTo(CHECK_CAN);
+                break;
+            default:
+                err = ERROR_INVALID_STATE;
+                SetStateTo(UNINIT);
+                break;
+        }
+        
+        if (err) DisplayErrorCode(err);
+        
+        if (DBG_UART_SpiUartGetRxBufferSize()) {
+            DebugPrint(DBG_UART_UartGetByte());
+        }
+        
+        CyDelay(100);
+        */
+        
+        if (CAN_time_LED > 0) {
+            LED_CAN_Write(0);
+            CAN_time_LED--;
+        } else {
+            LED_CAN_Write(0);
+        }
+            
+        uint32 c = DBG_UART_UartGetChar();
+        if (c) {
+            if (c == '\r') {
+                parseLine(&can_tx, uart_rx+1, uart_rx_len-1);
+                if (SendCANPacket(&can_tx) == 0) {
+                    sprintCANPacket(&can_tx, uart_tx);
+                    Print("sent ");
+                    Print(uart_tx);
+                    uart_rx_len = 0;
+                } else {
+                    Print("Epic FAIL\r\n");
+                }
+            } else {
+                uart_rx[uart_rx_len] = c;
+                uart_rx_len++;
+            }
+        }
+        
+        if (PollAndReceiveCANPacket(&can_rx) == ERROR_NONE) {
+            sprintCANPacket(&can_rx, uart_tx);
+            Print(uart_tx);
+        }
+        CyDelay(100);
+    }
+}
+
+void Initialize(void) {
+    CyGlobalIntEnable; /* Enable global interrupts. LED arrays need this first */
+    
+    // address = getSerialAddress();
+    
+    DBG_UART_Start();
+    // sprintf(txData, "Dip Addr: %x \r\n", address);
+    printf("starting program \n");
+    Print(txData);
+    
+    LED_DBG_Write(0);
+    
+    InitCAN(0x4, (int)address);
+    Timer_Period_Reset_Start();
+    
+    // isr_Button_1_StartEx(Button_1_Handler);
+    isr_Period_Reset_StartEx(Period_Reset_Handler);
+}
+
+void DebugPrint(char input) {
+    switch(input) {
+        case 'f':
+            sprintf(txData, "Mode: %x State:%x \r\n", GetMode(), GetState());
+            break;
+        case 'x':
+            sprintf(txData, "bruh\r\n");
+            break;
+        default:
+            sprintf(txData, "what\r\n");
+            break;
+    }
+    Print(txData);
+}
+
+void DisplayErrorCode(uint8_t code) {    
+    ERROR_time_LED = 0;
+    LED_ERR_Write(ON);
+    
+    sprintf(txData, "Error %X\r\n", code);
+    Print(txData);
+
+    switch(code)
+    {
+        case ERROR_INVALID_TTC:
+            Print("Cannot Send That Data Type!\n\r");
+            break;
+        default:
+            //some error
+            break;
+    }
+}
+
+/* [] END OF FILE */
